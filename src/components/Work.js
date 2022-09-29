fe9e8165f3dfd48fdca00bdc0d11791715b4f9a7
jrchan4 <justinrichardchan@gmail.com>
2022-09-29 02:37:38 -0700

    work content

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,31 @@ function Work() {
       <h1 className="work-title">Work Experience</h1>
       <div className="hero-container">
         <div className="hero-content">
-
+          <div className="visier">
+            <div className="visier-logo">
+              <h3>Visier Inc.</h3>
+              <img src="/assets/images/visier-logo.png" alt="visier logo"/>
+              <p>Software Developer Co-op</p>
+              <p className="team">Analysis & Visualization</p>
+              <p className="team">May 2022 - Present</p>
+            </div>
+            <div className="work-description">
+              <p>Engineered product features improving user experience and product value for 15,000+ customers, working alongside lead engineers to rapidly ship new functionality and close weekly production patch bugs.</p>
+            </div>
+          </div>
+          <div className="work-divider"></div>
+          <div className="rbc">
+            <div className="visier-logo">
+              <h3>Royal Bank of Canada</h3>
+              <img src="/assets/images/rbc-logo.png" alt="rbc logo"/>
+              <p>Software Engineer Co-op</p>
+              <p className="team">Data Services Team</p>
+              <p className="team">Sept 2020 - Aug 2021</p>
+            </div>
+            <div className="work-description">
+              <p>Developed Java microservices with Spring Boot on enterprise OpenShift environments to ingest log data into ElasticSearch, enabling centralized monitoring and adherence to data retention compliance.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
