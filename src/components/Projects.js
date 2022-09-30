e0122ee91ef3432b178ebdb459c59457f608fcd8
jrchan4 <justinrichardchan@gmail.com>
2022-09-29 17:22:44 -0700

    projects content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,4 @@
+import { FaLink, FaPlayCircle, FaInstagram, FaLinkedinIn, FaGithub} from "react-icons/fa";
 import './Projects.css';
 
 function Projects() {
@@ -7,47 +8,157 @@ function Projects() {
       <div className="projects-content">
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Club VP Tech</h3>
+              <div className="title-tag-divider"></div>
+              <h3>GDSC</h3>
+            </div>
+            <img className="project-img" src="/assets/images/gdsc-logo.png" alt="visier logo"/>
+            <p className="project-tags">Leadership, GCP, Cloud Architecture</p>
+            <p className="project-description">Joined the founding team to grow a community of 120+ students. Spearheaded the club’s backend infrastructure on GCP and presented technical workshops to 200+ students.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://ubcdsc.com/"><FaLink /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/dsc-ubc/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ubcdsc"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=h_VskGX85dc"><FaPlayCircle /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=VruTOetuP04"><FaPlayCircle /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Hackathon</h3>
+              <div className="title-tag-divider"></div>
+              <h3>MeetMe</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">Slack API, Flask, MariaDB</p>
+            <p className="project-description">Python slack bot for casual networking. Finalist in RBC innovation challenge, chosen to be advanced to internal production.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Tag</h3>
+              <div className="title-tag-divider"></div>
+              <h3>Title</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">tags</p>
+            <p className="project-description">Developer Student Clubs is a Google Developers program for university students to learn mobile and web development skills, design thinking skills and leadership skills.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Tag</h3>
+              <div className="title-tag-divider"></div>
+              <h3>Title</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">tags</p>
+            <p className="project-description">Developer Student Clubs is a Google Developers program for university students to learn mobile and web development skills, design thinking skills and leadership skills.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Tag</h3>
+              <div className="title-tag-divider"></div>
+              <h3>Title</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">tags</p>
+            <p className="project-description">Developer Student Clubs is a Google Developers program for university students to learn mobile and web development skills, design thinking skills and leadership skills.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Tag</h3>
+              <div className="title-tag-divider"></div>
+              <h3>Title</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">tags</p>
+            <p className="project-description">Developer Student Clubs is a Google Developers program for university students to learn mobile and web development skills, design thinking skills and leadership skills.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Tag</h3>
+              <div className="title-tag-divider"></div>
+              <h3>Title</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">tags</p>
+            <p className="project-description">Developer Student Clubs is a Google Developers program for university students to learn mobile and web development skills, design thinking skills and leadership skills.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Tag</h3>
+              <div className="title-tag-divider"></div>
+              <h3>Title</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">tags</p>
+            <p className="project-description">Developer Student Clubs is a Google Developers program for university students to learn mobile and web development skills, design thinking skills and leadership skills.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
         <div className="hero-container">
           <div className="hero-content">
-
+            <div className="title-tag">
+              <h3 className="project-type">Tag</h3>
+              <div className="title-tag-divider"></div>
+              <h3>Title</h3>
+            </div>
+            <img className="project-img" src="/assets/images/visier-logo.png" alt="visier logo"/>
+            <p className="project-tags">tags</p>
+            <p className="project-description">Developer Student Clubs is a Google Developers program for university students to learn mobile and web development skills, design thinking skills and leadership skills.</p>
+            <div className="project-links">
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/justinchan84/"><FaLinkedinIn /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/jrchan84"><FaGithub /></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jrchan.ca/"><FaInstagram /></a>
+            </div>
           </div>
         </div>
       </div>
